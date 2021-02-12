6cac15c7279c051a6d803ceb35b4d2ce803298f5
shopovec <a.shopovec@gmail.com>
2021-02-12 21:09:18 +0200

    my first commit

---
+++ home_work.js
@@ -0,0 +1,10 @@
+//Задача 1
+let nameUser = prompt('Введите имя');
+let userYear = parseInt(prompt('Введите год рождения'));
+
+let curYear = 2021;
+let result = curYear - userYear;
+
+document.write(nameUser + " тебе " + result + " лет");
+
+//Задача 2
---
+++ script.js
@@ -0,0 +1,55 @@
+//Задача 1
+let nameUser = prompt('Введите имя');
+let userYear = parseInt(prompt('Введите год рождения'));
+
+let curYear = 2021;
+let result = curYear - userYear;
+
+document.write(nameUser + " тебе " + result + " лет");
+
+
+// let num = parseInt(prompt('Введи номер'));
+//
+//
+// let a = num > 18 ? "Ok" : 'Not Ok' ;
+//
+//
+// document.write( a );
+
+
+/*
+
+parseInt();
+
+parseFloat();
+
+
+prompt('Введи число');
+
+var myVarFirst = 'jkkfdlgjdgkjdlgfd';
+
+console.log(typeof myVarFirst);
+
+myVarFirst = 50;
+
+console.log(typeof myVarFirst);
+
+
+test = 60;
+
+
+
+document.write(test);
+
+
+
+//let ob = {a:20};
+
+//document.write(ob);
+//alert('Hello');
+
+//confirm('ok');
+
+
+//console.log(ob);
+*/
