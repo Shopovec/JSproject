d226b45093b1369fdb6d44ef9bc4577f1568945a
shopovec <a.shopovec@gmail.com>
2021-02-17 21:43:27 +0200

    B-17-02

---
+++ script-17-02.js
@@ -0,0 +1,51 @@
+let num1 = 200;
+let num2 = 10;
+let num3 = 17;
+
+let result = num1 / (num2 * num3);
+
+//let res = result > 300 ? 'ok' : 'not ok';
+let res = '10';
+
+
+// if (res === 5) {
+//     alert('Num is 5');
+// } else if (res == 5) {
+//     alert('type ok')
+// }else if (res == 4) {
+//     alert('type 4')
+// } else if (res == 10) {
+//     alert('type 10')
+// }
+// else {
+//     alert('ok');
+// }
+//
+//
+// alert('before block');
+
+
+let day = parseInt(prompt('введи день недели'));
+console.log(day);
+switch (day) {
+    case 1:
+        alert('понедельник');
+        break;
+    case 2:
+        alert('вторник');
+        break;
+    case 3:
+        alert('среда');
+        break;
+    case 5:
+        alert('пятница');
+        break;
+    default:
+        alert('не такого дня');
+        break;
+}
+
+
+
+
+//document.write(res);
